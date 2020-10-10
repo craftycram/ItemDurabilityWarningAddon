cd40ad97d7b7ec7777dcffa6825bd0cb44f08047
craftycram <github@craftycram.net>
2020-10-10 21:24:05 +0200

    testing player interaction event

--- src/main/java/net/craftycram/durabilitywarningaddon/ClickListener.java
+++ src/main/java/net/craftycram/durabilitywarningaddon/ClickListener.java
@@ -4,6 +4,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 
@@ -13,19 +14,23 @@ public class ClickListener {
     private boolean delta;
 
     @SubscribeEvent
-    public void onMouseInputEvent(final InputEvent.MouseInputEvent event) {
+    public void onMouseInputEvent(final PlayerInteractEvent event) {
         final EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+        player.addChatMessage(new ChatComponentText("event"));
+        /*
         final ItemStack item = player.getCurrentEquippedItem();
         if (item == null) { return; }
         if (!item.isItemStackDamageable()) { return; };
         player.addChatMessage(new ChatComponentText((item.getMaxDamage() - item.getItemDamage()) + " - " + delta + " " + Main.warningDurability));
+        player.addChatMessage(new ChatComponentText(event.getPhase() + ""));
         if (Minecraft.getMinecraft().mouseHelper.deltaX == 0 && Minecraft.getMinecraft().mouseHelper.deltaY == 0 || delta) {
             delta = true;
             if (item.getMaxDamage() - item.getItemDamage() < Main.warningDurability) {
                 player.playSound("note.pling", 100, 0);
             }
         }
-        delta = Minecraft.getMinecraft().mouseHelper.deltaX == 0 && Minecraft.getMinecraft().mouseHelper.deltaY == 0;
+        */
+        // delta = Minecraft.getMinecraft().mouseHelper.deltaX == 0 && Minecraft.getMinecraft().mouseHelper.deltaY == 0;
     }
 
 }
